1c45634a2bf683d52ebffc7e5f779c87cd1731fd
hoanghan2005 <102842324+hoanghan2005@users.noreply.github.com>
2022-04-17 13:45:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "gửi đến ankk juuu của iemmm :3",
+    introTitle: 'Nguyễn Nhật Nam',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
